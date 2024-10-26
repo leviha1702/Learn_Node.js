f360df019f54d12a68f21f70dc06d205517c6324
diem <levietha170205@gmail.com>
2024-10-26 16:02:11 +0700

    practice with npm with API

--- express_app.js
+++ express_app.js
@@ -1,8 +1,39 @@
 const express = require ('express');
 const app = express();
+const bodyParser = require("body-parser");
+const { message } = require('statuses');
+app.use(bodyParser.json());
 //đinh ngĩa route gốc
 app.get('/',(req,res)=>{
     res.send('Chào mừng đến với API Node.js!');
+});
+app.get('/world',(req,res)=>{
+    console.log("Client doing call a API");
+    res.json({message:"Hello World!"});
+});
+app.post('/profile',(req,res)=>{
+    const {email,password} = req.body;
+    if(! email || !password){
+        return res.json({
+            message : "Email or Password is required",
+        });
+    }
+    if(Number(password) === 123456){
+        return res.json({
+        message : "Login success",
+        });
+    }
+    return res.json({
+        message: "Login fail",
+    });
+});
+app.get('/profile',(req,res)=>{
+    console.log(req.body);
+    res.json({
+        name:"Le Viet Ha",
+        age:19,
+        address:"Nha Trang",
+    });
 });
 //nghe tại cổng 3000
 app.listen(3000,()=>{
